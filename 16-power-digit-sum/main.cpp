fed148437f3c7f7b00266f25aea1e4ac65d638d7
uddeepk <uddeepk@gmail.com>
2021-03-04 23:39:49 -0900

    finished

--- 16-power-digit-sum/main.cpp
+++ 16-power-digit-sum/main.cpp
@@ -3,9 +3,42 @@
 using std::vector;
 using std::cout;
 
+int sumOfDigits (int n) {
+    int sum = 0;
+    while ( n != 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
 int main() {
-    int a = 2;
-    a = a << 1;
-    cout << a ;
+    // Create a vector of ints to store the number in different sections
+    vector <int> theNumber (500, 0);
+    theNumber[0] = 1;
+    int n = 1000;
+    for ( int i = 0 ; i < n; ++i ) {
+        // Multiply by 2, or left shift << 1
+        for ( auto &partial : theNumber) {
+            partial = partial << 1;
+        }
+        // Check if overflow. If yes then carry over overflow to higher index
+        int carry = 0;
+        for ( auto &partial : theNumber ) {
+            partial += carry;
+            if(partial >= 10000) {
+                carry = partial / 10000;
+                partial %= 10000;
+            } else {
+                carry = 0;
+            }
+        }
+    }
+
+    int answer = 0;
+
+    for (const auto &n : theNumber) {
+        answer += sumOfDigits(n);
+    }
+    cout << answer;
     return 0;
 }
